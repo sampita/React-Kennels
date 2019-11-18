9ea3807a6237915423ae81a9e0afbfedc9511de4
Sam Pita <samanthakpita@gmail.com>
2019-11-18 14:21:19 -0600

    added navbar

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -4,12 +4,14 @@ import AnimalCard from './animal/AnimalCard'
 import OwnerCard from './owner/OwnerCard'
 import LocationCard from './location/LocationCard'
 import EmployeeCard from './employee/EmployeeCard'
+import NavBar from './nav/NavBar'
 
 class Kennel extends Component {
     render() {
         return (
             /* a Render method can only return a single object, so everything is wrapped in one div */
             <div>
+                <NavBar />
                 <div>
                     <h2>Student Kennels<br />
                         <small>Loving care when you're not there.</small>
